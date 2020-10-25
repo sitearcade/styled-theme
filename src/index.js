1af69890c44fcd6fa8c5be13d8c6c77d8b0294bb
Mike Stop Continues <mikestopcontinues@gmail.com>
2020-10-25 11:45:29 -0500

    Fix export

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-/* eslint-disable no-duplicate-imports */
-
-// import
-
-import {api, curryArgs} from './api';
-
 // export
 
-export default curryArgs(api);
 export * from './api';
 export * from './styled';
