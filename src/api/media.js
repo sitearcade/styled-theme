1a5721bbb8a37a4c87580cbdd69b7072b76b0888
Mike Stop Continues <mikestopcontinues@gmail.com>
2020-10-26 12:55:42 -0500

    Add media tests and fixes

--- src/api/media.js
+++ src/api/media.js
@@ -4,8 +4,10 @@ import {map, memoizeWith, equals, is} from 'ramda';
 
 // fns
 
-const getMediaRanges = memoizeWith(equals, ([bps, px]) => {
-  const clip = 0.02 / px;
+const clipMax = (remPx, v) => v - (0.02 / remPx) ?? null;
+
+const getMediaRanges = memoizeWith(equals, ([bps, remPx]) => {
+  const clip = 0.02 / remPx;
   const mins = map((v) => v ?? null, bps);
   const maxs = map((v) => v - clip ?? null, bps);
 
@@ -14,17 +16,16 @@ const getMediaRanges = memoizeWith(equals, ([bps, px]) => {
 
 // export
 
-const media = (min = null, max = null) =>
-  ({theme: {remPx, breakpoints: bps}}) => {
-    const [mins, maxs] = getMediaRanges([bps, remPx]);
+const media = ({theme: {remPx, breakpoints: bps}}, min = null, max = null) => {
+  const [mins, maxs] = getMediaRanges([bps, remPx]);
 
-    min = is(Number, min) ? min : mins[min];
-    max = is(Number, max) ? max : maxs[max];
+  min = is(Number, min) ? min : mins[min];
+  max = is(Number, max) ? clipMax(remPx, max) : maxs[max];
 
-    return min && !max ? `@media (min-width: ${min}em)` :
-      !min && max ? `@media (max-width: ${max}em)` :
-      min && max ? `@media (min-width: ${min}em) and (max-width: ${max}em)` :
-      '@media ()';
-  };
+  return min && !max ? `@media (min-width: ${min}em)` :
+    !min && max ? `@media (max-width: ${max}em)` :
+    min && max ? `@media (min-width: ${min}em) and (max-width: ${max}em)` :
+    '@media ()';
+};
 
 export default media;
